6941e2dd18b193b480f9f0217a1d18d05a4df698
Mehmet <iresyak@gmail.com>
2020-03-31 16:25:56 +0200

    feat: pages created

---
+++ packages/client/src/pages/ErrorPage.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+const ErrorPage = () =>
+{
+    return (
+        <div>
+            Error Page
+        </div>
+    )
+};
+
+export default ErrorPage
---
+++ packages/client/src/pages/SignupPage.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+const SignupPage = () =>
+{
+    return (
+        <div>
+            Sign-up Page
+        </div>
+    )
+};
+
+export default SignupPage
