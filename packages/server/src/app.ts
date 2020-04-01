179ba813a74dfbb0c1c36523ae51eae0507cfb3a
ahmetsametoglu.fr@gmail.com <ahmetsametoglu.fr@gmail.com>
2020-04-01 14:43:43 +0200

    s-feat: create cors middleware and use

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -4,11 +4,13 @@ import { config } from './config';
 import { connect } from 'mongoose';
 import router from './routes';
 import { handleRouteError, handleError } from './middlewares/error.middleware';
+import { corsCredential } from './middlewares/cors.middleware';
 
 const app = express();
 
 // middleware
 app.use(bodyParser.json());
+app.use(corsCredential);
 
 //routes
 app.use(router);
