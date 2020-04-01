6e38902f50ab2748b0d2c33b759c0670b73a8ddc
ahmetsametoglu.fr@gmail.com <ahmetsametoglu.fr@gmail.com>
2020-04-01 15:23:55 +0200

    c-feat: implement redux action to login-form

--- packages/client/src/container/auth/LoginForm.tsx
+++ packages/client/src/container/auth/LoginForm.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   IonCard,
   IonCardHeader,
@@ -10,9 +10,26 @@ import {
   IonLabel,
   IonInput,
   IonButton,
+  IonSpinner,
 } from '@ionic/react';
+import { useDispatch } from 'react-redux';
+import { AuthActions } from 'src/redux/auth/action';
+import { useTypeSelector } from 'src/redux/helper/selector.helper';
+
+type State = {
+  username: string;
+  password: string;
+};
 
 const LoginForm = () => {
+  const { loading } = useTypeSelector(s => s.authState);
+  const dispatch = useDispatch();
+  const [state, setState] = useState<State>({ username: 'quatmer', password: '4mer' });
+
+  const login = () => {
+    dispatch(AuthActions.login(state.username, state.password));
+  };
+
   return (
     <IonCard>
       <IonCardHeader>
@@ -27,18 +44,28 @@ const LoginForm = () => {
               username
               <IonText color="danger"> *</IonText>
             </IonLabel>
-            <IonInput type="text" />
+            <IonInput
+              value={state.username}
+              type="text"
+              onIonChange={event => setState({ ...state, username: event.detail.value || '' })}
+            />
           </IonItem>
           <IonItem>
             <IonLabel position="stacked">
               password
               <IonText color="danger"> *</IonText>
             </IonLabel>
-            <IonInput type="password" />
+            <IonInput
+              value={state.password}
+              type="password"
+              onIonChange={event => setState({ ...state, password: event.detail.value || '' })}
+            />
           </IonItem>
         </IonList>
         <div id="auth-button-container" className="full-size content-center">
-          <IonButton color="tertiary">Login</IonButton>
+          <IonButton color="tertiary" onClick={login} disabled={loading}>
+            {loading ? <IonSpinner name="dots" /> : 'Login'}
+          </IonButton>
         </div>
 
         <IonButton id="other-auth-button" fill="clear" color="medium" size="small" routerLink="/signup">
