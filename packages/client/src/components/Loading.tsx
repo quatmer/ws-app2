f2bf0b0e2614d612446d5fce1e5abedd70da8a0d
ahmetsametoglu.fr@gmail.com <ahmetsametoglu.fr@gmail.com>
2020-04-01 09:51:29 +0200

    c-feat: create loading component

---
+++ packages/client/src/components/Loading.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { IonSpinner } from '@ionic/react';
+
+const Loading = () => {
+  return (
+    <div id="loading-spinner">
+      <IonSpinner />
+    </div>
+  );
+};
+
+export default Loading;
