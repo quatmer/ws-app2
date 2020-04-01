7bc7b05678e48659d42316095e810c57f9ef9eee
ahmetsametoglu.fr@gmail.com <ahmetsametoglu.fr@gmail.com>
2020-04-01 15:21:22 +0200

    c-feat: create store provider

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -2,7 +2,13 @@ import './App.scss';
 import React from 'react';
 
 import AppInit from './AppInit';
+import { Provider } from 'react-redux';
+import { configureStore } from './redux/store';
 
-const App: React.FC = () => <AppInit />;
+const App: React.FC = () => (
+  <Provider store={configureStore()}>
+    <AppInit />;
+  </Provider>
+);
 
 export default App;
