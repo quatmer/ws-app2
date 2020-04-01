4d72d30c288845dc117339472f41347a3b0e2824
ahmetsametoglu.fr@gmail.com <ahmetsametoglu.fr@gmail.com>
2020-04-01 08:36:43 +0200

    C-feat: initialize http service

--- packages/client/src/AppInit.tsx
+++ packages/client/src/AppInit.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Routing from './router/Routing';
 import { IonApp } from '@ionic/react';
+import { HttpService } from './api/services/http.service';
 
 const AppInit = () => {
+  useEffect(() => {
+    HttpService.initializeAxios();
+    HttpService.startLogging();
+  }, []);
+
   return (
     <IonApp>
       <Routing />
