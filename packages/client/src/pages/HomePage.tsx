01a0101af6ec9e1124283d5d0cb260e9fef52bc0
ahmetsametoglu.fr@gmail.com <ahmetsametoglu.fr@gmail.com>
2020-04-01 17:17:56 +0200

    c-feat: redesign home-page and add logout button

--- packages/client/src/pages/HomePage.tsx
+++ packages/client/src/pages/HomePage.tsx
@@ -1,12 +1,31 @@
-import React from 'react'
+import React from 'react';
+import { IonPage, IonHeader, IonTitle, IonToolbar, IonContent, IonButton, IonButtons, IonIcon } from '@ionic/react';
+import { logOutSharp } from 'ionicons/icons';
+import { useDispatch } from 'react-redux';
+import { AuthActions } from 'src/redux/auth/action';
 
-const HomePage = () =>
-{
-    return (
-        <div>
-            Home Page
-        </div>
-    )
+const HomePage = () => {
+  const dispatch = useDispatch();
+
+  const logout = () => {
+    dispatch(AuthActions.logout());
+  };
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar color="tertiary">
+          <IonTitle>Home Page</IonTitle>
+          <IonButtons slot="end">
+            <IonButton onClick={logout}>
+              <IonIcon slot="icon-only" icon={logOutSharp} />
+            </IonButton>
+          </IonButtons>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent></IonContent>
+    </IonPage>
+  );
 };
 
-export default HomePage
+export default HomePage;
