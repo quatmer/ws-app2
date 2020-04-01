d389efd55d36f8ffc70d75c3064822a5c238f69e
ahmetsametoglu.fr@gmail.com <ahmetsametoglu.fr@gmail.com>
2020-04-01 17:16:23 +0200

    c-feat: save user to local storage

--- packages/client/src/api/services/auth.service.ts
+++ packages/client/src/api/services/auth.service.ts
@@ -1,11 +1,25 @@
-export const AuthServices={
+import { IUser } from '../../../../shared/models/user';
 
-    saveToken:(token:string)=>{
-        localStorage.setItem('authToken',token);
-    },
+export const AuthServices = {
+  setToken: (token: string) => {
+    localStorage.setItem('authToken', token);
+  },
 
-    getToken:()=>{
-        localStorage.getItem('authToken');
+  getToken: () => {
+    localStorage.getItem('authToken');
+  },
+
+  setUser: (user: IUser | null) => {
+    if (user) {
+      const userData = JSON.stringify(user);
+      localStorage.setItem('user', userData);
+    } else {
+      localStorage.removeItem('user');
     }
+  },
 
-}
+  getUser: (): IUser | null => {
+    const userData = localStorage.getItem('user');
+    return !!userData ? JSON.parse(userData) : null;
+  },
+};
