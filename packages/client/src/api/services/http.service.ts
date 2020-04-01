0d7f2892c7cfa99c4fb13456379b4d6a780bd419
ahmetsametoglu.fr@gmail.com <ahmetsametoglu.fr@gmail.com>
2020-04-01 08:34:35 +0200

    C-feat: create http logger function

--- packages/client/src/api/services/http.service.ts
+++ packages/client/src/api/services/http.service.ts
@@ -4,4 +4,24 @@ export const HttpService = {
   initializeAxios: () => {
     Axios.defaults.baseURL = ' http://localhost:3000';
   },
+
+  startLogging: () => {
+    Axios.interceptors.request.use(request => {
+      console.log(
+        `%cRequest: ${request.method} ${request.baseURL}${request.url}`,
+        `color: red; font-weight: bold;`,
+        request.data,
+      );
+      return request;
+    });
+    Axios.interceptors.response.use(response => {
+      console.log(
+        `%cResponse: ${response.config.method} ${response.config.baseURL}${response.config.url}`,
+        `color: green; font-weight: bold;`,
+        { status: response?.status, text: response?.statusText },
+        response.data,
+      );
+      return response;
+    });
+  },
 };
