d7535d5cf1ccf6d212b182aab6cc80af340e1950
Serdar YILMAZ <serdaryilmaz1323@gmail.com>
2020-04-02 16:29:40 +0200

    c-fix: refresh product categories list

--- packages/client/src/container/ProductCategoryList/ProductCategoryList.tsx
+++ packages/client/src/container/ProductCategoryList/ProductCategoryList.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useTypeSelector } from 'src/redux/helper/selector.helper';
 import { IonGrid, IonRow, IonCol } from '@ionic/react';
 import ProductCategoryListItem from './ProductCategoryListItem';
+import { useDispatch } from 'react-redux';
+import { ProductCategoryActions } from '../../redux/product-category/action';
 
 const ProductCategoryList = () => {
   const { categories } = useTypeSelector(c => c.productCategoryState);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(ProductCategoryActions.getList());
+    // eslint-disable-next-line
+  }, []);
 
   return (
     <IonGrid>
