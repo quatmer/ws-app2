cc8b60833320e525aaafa78646a34b96202d4b4b
ahmetsametoglu.fr@gmail.com <ahmetsametoglu.fr@gmail.com>
2020-04-02 10:09:03 +0200

    c-feat: create side menu component

---
+++ packages/client/src/components/SideMenu.tsx
@@ -0,0 +1,66 @@
+import {
+  IonContent,
+  IonIcon,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonListHeader,
+  IonMenu,
+  IonMenuToggle,
+  IonNote,
+} from '@ionic/react';
+import React from 'react';
+import { RouteComponentProps, withRouter } from 'react-router-dom';
+import { mailOutline, mailSharp } from 'ionicons/icons';
+import './SideMenu.scss';
+
+interface MenuProps extends RouteComponentProps {
+  selectedPage: string;
+}
+
+interface AppPage {
+  url: string;
+  iosIcon: string;
+  mdIcon: string;
+  title: string;
+}
+
+const appPages: AppPage[] = [
+  {
+    title: 'Home',
+    url: '/home',
+    iosIcon: mailOutline,
+    mdIcon: mailSharp,
+  },
+];
+
+const Menu: React.FunctionComponent<MenuProps> = ({ selectedPage }) => {
+  return (
+    <IonMenu contentId="main" type="overlay">
+      <IonContent>
+        <IonList id="inbox-list">
+          <IonListHeader>quatmer</IonListHeader>
+          <IonNote>quatmer@quatmer.com</IonNote>
+          {appPages.map((appPage, index) => {
+            return (
+              <IonMenuToggle key={index} autoHide={false}>
+                <IonItem
+                  className={selectedPage === appPage.title ? 'selected' : ''}
+                  routerLink={appPage.url}
+                  routerDirection="none"
+                  lines="none"
+                  detail={false}
+                >
+                  <IonIcon slot="start" icon={appPage.iosIcon} />
+                  <IonLabel>{appPage.title}</IonLabel>
+                </IonItem>
+              </IonMenuToggle>
+            );
+          })}
+        </IonList>
+      </IonContent>
+    </IonMenu>
+  );
+};
+
+export default withRouter(Menu);
