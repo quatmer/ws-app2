8eecfd366ac44ab3f008eb138db6974c9b98aed5
Mehmet <iresyak@gmail.com>
2020-04-02 13:44:37 +0200

    c-fix: redux refactor

--- packages/client/src/redux/product-category/action.ts
+++ packages/client/src/redux/product-category/action.ts
@@ -3,31 +3,30 @@ import { ActionsUnion } from '../helper/type.helper';
 import { IProductCategory } from '../../../../shared/models/product-category';
 
 export enum ProductCategoryActionType {
-  CREATE = '[product-category]: CREATE',
-  CREATE_SUCCESS = '[product-category]: CREATE_SUCCESS',
-  CREATE_ERROR = '[product-category]: CREATE_ERROR',
-  UPDATE = '[product-category]: UPDATE',
-  UPDATE_SUCCESS = '[product-category]: UPDATE_SUCCESS',
-  UPDATE_ERROR = '[product-category]: UPDATE_ERROR',
-  DELETE = '[product-category]: DELETE',
-  DELETE_SUCCESS = '[product-category]: DELETE_SUCCESS',
-  DELETE_ERROR = '[product-category]: DELETE_ERROR',
+  CREATE_UPDATE = '[product category]: CREATE_UPDATE',
+  CREATE_UPDATE_SUCCESS = '[product category]: CREATE_UPDATE_SUCCESS',
+  CREATE_UPDATE_ERROR = '[product category]: CREATE_UPDATE_ERROR',
+  DELETE = '[product category]: DELETE',
+  DELETE_SUCCESS = '[product category]: DELETE_SUCCESS',
+  DELETE_ERROR = '[product category]: DELETE_ERROR',
+  GET_LIST = '[product category]: GET_LIST',
+  GET_LIST_SUCCESS = '[product category]: GET_LIST_SUCCESS',
+  GET_LIST_ERROR = '[product category]: GET_LIST_ERROR',
 }
 
 export const ProductCategoryActions = {
-  create: (name: string, categoryTree: IProductCategory) => action(ProductCategoryActionType.CREATE, { name }),
-  createSuccess: (productCategory: IProductCategory) =>
-    action(ProductCategoryActionType.CREATE_SUCCESS, { productCategory }),
-  createError: (message: string) => action(ProductCategoryActionType.CREATE_ERROR, { message }),
-
-  update: (name: string, categoryTree: IProductCategory) => action(ProductCategoryActionType.UPDATE, { name }),
-  updateSuccess: (productCategory: IProductCategory) =>
-    action(ProductCategoryActionType.UPDATE_SUCCESS, { productCategory }),
-  updateError: (message: string) => action(ProductCategoryActionType.UPDATE_ERROR, { message }),
+  createUpdate: (category: IProductCategory) => action(ProductCategoryActionType.CREATE_UPDATE, { category }),
+  createUpdateSuccess: (category: IProductCategory) =>
+    action(ProductCategoryActionType.CREATE_UPDATE_SUCCESS, { category }),
+  createUpdateError: (message: string) => action(ProductCategoryActionType.CREATE_UPDATE_ERROR, { message }),
 
   delete: (id: string) => action(ProductCategoryActionType.DELETE, { id }),
-  deleteSuccess: (message: string) => action(ProductCategoryActionType.DELETE_SUCCESS, { message }),
+  deleteSuccess: (id: string) => action(ProductCategoryActionType.DELETE_SUCCESS, { id }),
   deleteError: (message: string) => action(ProductCategoryActionType.DELETE_ERROR, { message }),
+
+  getList: () => action(ProductCategoryActionType.GET_LIST),
+  getListSuccess: (categories: IProductCategory[]) => action(ProductCategoryActionType.GET_LIST_SUCCESS, { categories }),
+  getListError: (message: string) => action(ProductCategoryActionType.GET_LIST_ERROR, { message }),
 };
 
 export type ProductCategoryFuncType = ActionsUnion<typeof ProductCategoryActions>;
--- packages/client/src/redux/product-category/reducer.ts
+++ packages/client/src/redux/product-category/reducer.ts
@@ -4,13 +4,13 @@ import { ProductCategoryActionType, ProductCategoryFuncType } from './action';
 
 export type ProductCategoryStateType = {
   loading: boolean;
-  productCategory: IProductCategory | null;
+  categories: IProductCategory[];
   error: string | null;
 };
 
 const initialState: ProductCategoryStateType = {
   loading: false,
-  productCategory: null,
+  categories: [],
   error: null,
 };
 
@@ -19,25 +19,14 @@ export const productCategoryReducer: Reducer<ProductCategoryStateType, ProductCa
   action,
 ): ProductCategoryStateType => {
   switch (action.type) {
-    case ProductCategoryActionType.CREATE: {
-      return { ...state, loading: true, productCategory: null, error: null };
+    case ProductCategoryActionType.CREATE_UPDATE: {
+        return { ...state, loading: true, error: null };
     }
-    case ProductCategoryActionType.CREATE_SUCCESS: {
-      const { productCategory } = action.payload;
-      return { ...state, loading: false, productCategory: productCategory, error: null };
+    case ProductCategoryActionType.CREATE_UPDATE_SUCCESS: {
+      const { category} = action.payload;
+      return { ...state, loading: false, productCategory: category, error: null };
     }
-    case ProductCategoryActionType.CREATE_ERROR: {
-      const { message } = action.payload;
-      return { ...state, loading: false, productCategory: null, error: message };
-    }
-    case ProductCategoryActionType.UPDATE: {
-      return { ...state, loading: true, productCategory: null, error: null };
-    }
-    case ProductCategoryActionType.UPDATE_SUCCESS: {
-      const { productCategory } = action.payload;
-      return { ...state, loading: false, productCategory: productCategory, error: null };
-    }
-    case ProductCategoryActionType.UPDATE_ERROR: {
+    case ProductCategoryActionType.CREATE_UPDATE_ERROR: {
       const { message } = action.payload;
       return { ...state, loading: false, productCategory: null, error: message };
     }
