c74e5b8a25f08cbe8dd0daa0350840ff43b71069
Serdar YILMAZ <serdaryilmaz1323@gmail.com>
2020-04-02 14:31:31 +0200

    s-fix: The problem of children of product-category-schema has been resolved

--- packages/server/src/entities/product-category.entity.ts
+++ packages/server/src/entities/product-category.entity.ts
@@ -2,15 +2,17 @@ import { Schema, Document, model } from 'mongoose';
 import mongooseUniqueValidator = require('mongoose-unique-validator');
 import { IProductCategoryDocument } from '@shared/models/product-category';
 
-const ProductCategoryChildSchema = new Schema({ name: String });
-
 const ProductCategorySchema = new Schema(
   {
-    child: ProductCategoryChildSchema,
-    children: [ProductCategoryChildSchema],
+    name: String,
+    productCount: Number,
   },
   { timestamps: true, versionKey: false, bufferCommands: false },
 );
+
+ProductCategorySchema.add({
+  children: [ProductCategorySchema],
+});
 
 mongooseUniqueValidator(ProductCategorySchema);
 
