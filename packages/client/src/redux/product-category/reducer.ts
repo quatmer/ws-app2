77081ddce2fd7f21e0b4dc22188b19f5c26729ce
ahmetsametoglu.fr@gmail.com <ahmetsametoglu.fr@gmail.com>
2020-04-02 14:27:08 +0200

    c-feat: create product-category store

--- packages/client/src/redux/product-category/reducer.ts
+++ packages/client/src/redux/product-category/reducer.ts
@@ -20,25 +20,67 @@ export const productCategoryReducer: Reducer<ProductCategoryStateType, ProductCa
 ): ProductCategoryStateType => {
   switch (action.type) {
     case ProductCategoryActionType.CREATE_UPDATE: {
-        return { ...state, loading: true, error: null };
+      const { category } = action.payload;
+      const categories = [...state.categories];
+      const index = categories.findIndex(c => c._id === category._id);
+      if (index === -1) {
+        categories.push(category);
+      } else {
+        categories[index] = category;
+      }
+      return { ...state, categories: [...categories], loading: true, error: null };
     }
     case ProductCategoryActionType.CREATE_UPDATE_SUCCESS: {
-      const { category} = action.payload;
-      return { ...state, loading: false, productCategory: category, error: null };
+      const { category, refId } = action.payload;
+      const categories = [...state.categories];
+      const index = categories.findIndex(c => c._id === category._id);
+      if (index === -1) {
+        const newCatIndex = categories.findIndex(c => c._id === refId);
+        if (newCatIndex > -1) {
+          categories[newCatIndex] = category;
+        }
+      } else {
+        categories[index] = category;
+      }
+
+      return { ...state, categories: [...categories], loading: false, error: null };
     }
     case ProductCategoryActionType.CREATE_UPDATE_ERROR: {
-      const { message } = action.payload;
-      return { ...state, loading: false, productCategory: null, error: message };
+      const { message, category, refId } = action.payload;
+      let categories = [...state.categories];
+      if (!category._id) {
+        categories = categories.filter(c => c._id !== refId);
+      } else {
+        const index = categories.findIndex(c => c._id === category._id);
+        categories[index] = category;
+      }
+      return { ...state, loading: false, categories: [...categories], error: message };
     }
     case ProductCategoryActionType.DELETE: {
-      return { ...state, loading: true, productCategory: null, error: null };
+      return { ...state, loading: true, error: null };
     }
     case ProductCategoryActionType.DELETE_SUCCESS: {
-      return { ...state, loading: false, productCategory: null, error: null };
+      const { id } = action.payload;
+      let categories = [...state.categories];
+      const index = categories.findIndex(c => c._id === id);
+      categories.splice(index, 1);
+
+      return { ...state, loading: false, categories: [...categories], error: null };
     }
     case ProductCategoryActionType.DELETE_ERROR: {
       const { message } = action.payload;
-      return { ...state, loading: false, productCategory: null, error: message };
+      return { ...state, loading: false, error: message };
+    }
+    case ProductCategoryActionType.GET_LIST: {
+      return { ...state, loading: true, error: null };
+    }
+    case ProductCategoryActionType.GET_LIST_SUCCESS: {
+      const { categories } = action.payload;
+      return { ...state, loading: false, categories: [...categories], error: null };
+    }
+    case ProductCategoryActionType.GET_LIST_ERROR: {
+      const { message } = action.payload;
+      return { ...state, loading: false, error: message };
     }
     default:
       return { ...state };
