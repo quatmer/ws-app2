c0c08897a25d51e215cfab1207fb68e3366845ab
Serdar YILMAZ <serdaryilmaz1323@gmail.com>
2020-04-02 16:30:52 +0200

    c-feat: call initApp function

--- packages/client/src/AppInit.tsx
+++ packages/client/src/AppInit.tsx
@@ -1,12 +1,11 @@
 import React, { useEffect } from 'react';
 import Routing from './router/Routing';
 import { IonApp } from '@ionic/react';
-import { HttpService } from './api/services/http.service';
+import { AppService } from './api/services/app.service';
 
 const AppInit = () => {
   useEffect(() => {
-    HttpService.initializeAxios();
-    HttpService.startLogging();
+    AppService.initApp();
   }, []);
 
   return (
