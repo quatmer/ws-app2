537c225d86f0be0e6e2440d1b1d02e2fbb764866
Mehmet <iresyak@gmail.com>
2020-04-02 09:59:57 +0200

    c-feat: create productCategory redux

---
+++ packages/client/src/redux/product-category/action.ts
@@ -0,0 +1,33 @@
+import { action } from 'typesafe-actions';
+import { ActionsUnion } from '../helper/type.helper';
+import { IProductCategory } from '../../../../shared/models/product-category';
+
+export enum ProductCategoryActionType {
+  CREATE = '[product-category]: CREATE',
+  CREATE_SUCCESS = '[product-category]: CREATE_SUCCESS',
+  CREATE_ERROR = '[product-category]: CREATE_ERROR',
+  UPDATE = '[product-category]: UPDATE',
+  UPDATE_SUCCESS = '[product-category]: UPDATE_SUCCESS',
+  UPDATE_ERROR = '[product-category]: UPDATE_ERROR',
+  DELETE = '[product-category]: DELETE',
+  DELETE_SUCCESS = '[product-category]: DELETE_SUCCESS',
+  DELETE_ERROR = '[product-category]: DELETE_ERROR',
+}
+
+export const ProductCategoryActions = {
+  create: (name: string, categoryTree: IProductCategory) => action(ProductCategoryActionType.CREATE, { name }),
+  createSuccess: (productCategory: IProductCategory) =>
+    action(ProductCategoryActionType.CREATE_SUCCESS, { productCategory }),
+  createError: (message: string) => action(ProductCategoryActionType.CREATE_ERROR, { message }),
+
+  update: (name: string, categoryTree: IProductCategory) => action(ProductCategoryActionType.UPDATE, { name }),
+  updateSuccess: (productCategory: IProductCategory) =>
+    action(ProductCategoryActionType.UPDATE_SUCCESS, { productCategory }),
+  updateError: (message: string) => action(ProductCategoryActionType.UPDATE_ERROR, { message }),
+
+  delete: (name: string, categoryTree: IProductCategory) => action(ProductCategoryActionType.DELETE, { name }),
+  deleteSuccess: (message: string) => action(ProductCategoryActionType.DELETE_SUCCESS, { message }),
+  deleteError: (message: string) => action(ProductCategoryActionType.DELETE_ERROR, { message }),
+};
+
+export type ProductCategoryFuncType = ActionsUnion<typeof ProductCategoryActions>;
---
+++ packages/client/src/redux/product-category/reducer.ts
@@ -0,0 +1,57 @@
+import { IProductCategory } from '../../../../shared/models/product-category';
+import { Reducer, action } from 'typesafe-actions';
+import { ProductCategoryActionType, ProductCategoryFuncType } from './action';
+
+export type ProductCategoryStateType = {
+  loading: boolean;
+  productCategory: IProductCategory | null;
+  error: string | null;
+};
+
+const initialState: ProductCategoryStateType = {
+  loading: false,
+  productCategory: null,
+  error: null,
+};
+
+export const productCategoryReducer: Reducer<ProductCategoryStateType, ProductCategoryFuncType> = (
+  state = initialState,
+  action,
+): ProductCategoryStateType => {
+  switch (action.type) {
+    case ProductCategoryActionType.CREATE: {
+      return { ...state, loading: true, productCategory: null, error: null };
+    }
+    case ProductCategoryActionType.CREATE_SUCCESS: {
+      const { productCategory } = action.payload;
+      return { ...state, loading: false, productCategory: productCategory, error: null };
+    }
+    case ProductCategoryActionType.CREATE_ERROR: {
+      const { message } = action.payload;
+      return { ...state, loading: false, productCategory: null, error: message };
+    }
+    case ProductCategoryActionType.UPDATE: {
+      return { ...state, loading: true, productCategory: null, error: null };
+    }
+    case ProductCategoryActionType.UPDATE_SUCCESS: {
+      const { productCategory } = action.payload;
+      return { ...state, loading: false, productCategory: productCategory, error: null };
+    }
+    case ProductCategoryActionType.UPDATE_ERROR: {
+      const { message } = action.payload;
+      return { ...state, loading: false, productCategory: null, error: message };
+    }
+    case ProductCategoryActionType.DELETE: {
+      return { ...state, loading: true, productCategory: null, error: null };
+    }
+    case ProductCategoryActionType.DELETE_SUCCESS: {
+      return { ...state, loading: false, productCategory: null, error: null };
+    }
+    case ProductCategoryActionType.DELETE_ERROR: {
+      const { message } = action.payload;
+      return { ...state, loading: false, productCategory: null, error: message };
+    }
+    default:
+      return { ...state };
+  }
+};
