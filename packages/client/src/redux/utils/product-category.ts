3fdad1ca4d247dff7d260bf5ac12a30f01ede6c5
ahmetsametoglu.fr@gmail.com <ahmetsametoglu.fr@gmail.com>
2020-04-03 15:26:14 +0200

    c-fix: close same level selected product-category-item

--- packages/client/src/redux/utils/product-category.ts
+++ packages/client/src/redux/utils/product-category.ts
@@ -4,6 +4,7 @@ export const findAndToggleCategory = (id: string, categories: ProductCategoryDTO
   const toggledCategories = [...categories];
   categories.forEach(category => {
     if (category._id === id) {
+      categories.filter(c => c._id !== id).forEach(c => (c.isSelected = false));
       category.isSelected = !!!category.isSelected;
     } else {
       findAndToggleCategory(id, category.children);
