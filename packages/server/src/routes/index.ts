fdd3b0498a9f6c33098a3320f3484f012948f298
Serdar YILMAZ <serdaryilmaz1323@gmail.com>
2020-04-04 21:49:50 +0200

    s-feat: '/product' route implemented

--- packages/server/src/routes/index.ts
+++ packages/server/src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import authRouter from './auth.route';
 import productCategoryRouter from './product-category.route';
+import productRouter from './product-route';
 
 const router = Router();
 
 router.use('/auth', authRouter);
 router.use('/product-category', productCategoryRouter);
+router.use('/product', productRouter);
 
 export default router;
