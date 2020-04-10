9e8148d49f469b91845e23ef433c4e459bbfebcb
Serdar YILMAZ <serdaryilmaz1323@gmail.com>
2020-04-10 18:11:52 +0200

    c-feat: implement brand-service

--- packages/client/src/api/context/ServiceContext.tsx
+++ packages/client/src/api/context/ServiceContext.tsx
@@ -1,9 +1,11 @@
 import React, { createContext, FC, useContext, useEffect } from 'react';
 import { AuthService } from './services/auth.service';
 import { useDispatch } from 'react-redux';
+import { BrandService } from './services/brand.service';
 
 type Services = {
   Auth: AuthService;
+  brandService: BrandService
 };
 
 const ServiceContext = createContext<Services>({} as Services);
@@ -21,9 +23,11 @@ export const ServiceProvider: FC = props => {
 
   const services: Services = {
     Auth: new AuthService(dispatch),
+    brandService: new BrandService(dispatch)
   };
 
   return <ServiceContext.Provider value={services}>{props.children}</ServiceContext.Provider>;
 };
 
 export const useServices = () => useContext(ServiceContext);
+
