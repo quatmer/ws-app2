5b29192af8c7acbea87a70f459b725ec8b88148e
Serdar YILMAZ <serdaryilmaz1323@gmail.com>
2020-04-10 13:52:35 +0200

    s-feat: implement brand-reducer

--- packages/client/src/redux/combine.ts
+++ packages/client/src/redux/combine.ts
@@ -1,3 +1,5 @@
+import { brandEpics } from './brand/epic';
+import { brandReducer } from './brand/reducer';
 import { appReducer } from './app/reducer';
 import { combineEpics } from 'redux-observable';
 import { combineReducers } from 'redux';
@@ -6,10 +8,11 @@ import { authEpics } from './auth/epic';
 import { productCategoryReducer } from './product-category/reducer';
 import { productCategoryEpics } from './product-category/epic';
 
-export const rootEpic = combineEpics(...authEpics, ...productCategoryEpics);
+export const rootEpic = combineEpics(...authEpics, ...productCategoryEpics, ...brandEpics);
 
 export const rootReducer = combineReducers({
   authState: authReducer,
   appState: appReducer,
   productCategoryState: productCategoryReducer,
+  brandState: brandReducer,
 });
