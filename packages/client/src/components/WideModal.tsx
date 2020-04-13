4a8325167b3fd96fc085017ced73b87afcd069fd
Serdar YILMAZ <serdaryilmaz1323@gmail.com>
2020-04-13 21:55:03 +0200

    c-fix: solved problems in wide-modal

--- packages/client/src/components/WideModal.tsx
+++ packages/client/src/components/WideModal.tsx
@@ -1,37 +1,30 @@
 import './Component.scss';
 import React, { FC, useRef } from 'react';
-import {
-  IonModal,
-  IonButton,
-  IonIcon,
-  IonPage,
-  IonHeader,
-  IonToolbar,
-  IonTitle,
-  IonButtons,
-  IonContent,
-} from '@ionic/react';
+import { IonModal, IonButton, IonIcon, IonNote, } from '@ionic/react';
 import { close } from 'ionicons/icons';
 
-type Props = { isOpen: boolean; onDidDismiss: () => void; title: string };
+type Props = { isOpen: boolean; onDidDismiss: () => void; title: string, description?: string };
 const WideModal: FC<Props> = props => {
   const ref = useRef<HTMLIonModalElement | null>(null);
 
   return (
-    <IonModal ref={ref} id="wide-modal" isOpen={props.isOpen} onDidDismiss={props.onDidDismiss}>
-      <IonPage>
-        <IonHeader>
-          <IonToolbar color="tertiary">
-            <IonTitle>{props.title}</IonTitle>
-            <IonButtons slot="end">
-              <IonButton onClick={() => ref.current?.dismiss()}>
-                <IonIcon slot="icon-only" icon={close} />
-              </IonButton>
-            </IonButtons>
-          </IonToolbar>
-        </IonHeader>
-        <IonContent>{props.children}</IonContent>
-      </IonPage>
+    <IonModal id="wide-modal" keyboardClose isOpen={props.isOpen} onDidDismiss={props.onDidDismiss} ref={ref}>
+      <div id="modal">
+        <div id="toolbar">
+          <div id="title">
+            <h3>{props.title}</h3>
+          </div>
+          <div id="close-button">
+            <IonButton color="light" fill="clear" shape="round" onClick={() => ref.current?.dismiss()}>
+              <IonIcon slot="icon-only" icon={close} />
+            </IonButton>
+          </div>
+        </div>
+        <div id="content">
+          {props.description && <IonNote id="description">{props.description}</IonNote>}
+          {props.children}
+        </div>
+      </div>
     </IonModal>
   );
 };
