73052686cbd3e22a30a71de8caceaa677aa74fda
Mehmet <iresyak@gmail.com>
2020-04-13 16:37:13 +0200

    s-imports recreated

--- packages/server/src/routes/index.ts
+++ packages/server/src/routes/index.ts
@@ -2,13 +2,13 @@ import { Router } from 'express';
 import authRouter from './auth.route';
 import productCategoryRouter from './product-category.route';
 import productRouter from './product-route';
-import brandRouter from './brand.route';
+import productBrandRouter from './product-brand.route';
 
 const router = Router();
 
 router.use('/auth', authRouter);
 router.use('/product-category', productCategoryRouter);
-router.use('/brand', brandRouter);
+router.use('/brand', productBrandRouter);
 router.use('/product', productRouter);
 
 export default router;
