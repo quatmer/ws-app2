a0d0783ba299059a71ebd59d9cceb7049269c7e9
ahmetsametoglu.fr@gmail.com <ahmetsametoglu.fr@gmail.com>
2020-04-13 13:47:56 +0200

    feat: refresh token

--- packages/client/src/AppInit.tsx
+++ packages/client/src/AppInit.tsx
@@ -3,10 +3,12 @@ import Routing from './router/Routing';
 import { IonApp } from '@ionic/react';
 import { AppService } from './api/services/app.service';
 import NotificationContainer from './container/Notification/NotificationContainer';
+import { useServices } from './api/context/ServiceContext';
 
 const AppInit = () => {
+  const { Auth } = useServices();
   useEffect(() => {
-    AppService.initApp();
+    AppService.initApp(Auth);
   }, []);
 
   return (
--- packages/client/src/api/services/http.service.ts
+++ packages/client/src/api/services/http.service.ts
@@ -1,10 +1,34 @@
 import Axios from 'axios';
 import { AuthUtils } from '../utils/auth.util';
+import { AuthService } from '../context/services/auth.service';
 
 export const HttpService = {
   initializeAxios: () => {
     Axios.defaults.baseURL = ' http://localhost:3000';
     Axios.defaults.headers.common['auth'] = AuthUtils.getToken();
+  },
+
+  checkToken: (Auth: AuthService) => {
+    Axios.interceptors.response.use(
+      response => {
+        const token = response.config.headers['authxx'];
+        const fireToken = response.config.headers['fire-auth'];
+        // console.log(axios.defaults.headers.get('fire-auth'));
+        // console.log(response.headers.get('fire-auth'));
+
+        console.log({ token, fireToken }, { response });
+
+        return response;
+      },
+      error => {
+        if (!!error.response && error.response.status === 401) {
+          Auth.logout();
+        }
+
+        // return Error object with Promise
+        return Promise.reject(error);
+      },
+    );
   },
 
   startLogging: () => {
