c67fc3d8c89bad10efb68ddc48d3bb8cb3757c0a
Serdar YILMAZ <serdaryilmaz1323@gmail.com>
2020-04-13 21:57:44 +0200

    s-feat: create product-page component

--- packages/client/src/pages/ProductPage.tsx
+++ packages/client/src/pages/ProductPage.tsx
@@ -1,11 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import WideModal from '../components/WideModal';
+import { IonPage, IonHeader, IonToolbar, IonButtons, IonMenuButton, IonTitle, IonContent, IonButton, IonIcon } from '@ionic/react';
+import ProductBrandList from 'src/container/ProductBrand/ProductBrandList';
+import { add } from 'ionicons/icons';
+import NewProduct from 'src/container/Product/components/NewProduct';
 
 const ProductPage = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  }
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  }
+
   return (
-    <div>
-      <WideModal title="New Product" isOpen={true} onDidDismiss={() => {}} />
-    </div>
+
+    <IonPage>
+      <IonHeader>
+        <IonToolbar color="tertiary">
+          <IonButtons slot="end">
+            <IonButton onClick={openModal} >Add New Product
+            <IonIcon slot="start" icon={add} />
+            </IonButton>
+          </IonButtons>
+          <IonTitle>Product</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+
+        <WideModal title="New Product" isOpen={isModalOpen} onDidDismiss={() => { closeModal(); }} >
+          <NewProduct />
+        </WideModal>
+      </IonContent>
+    </IonPage>
+
+
+
   );
 };
 
