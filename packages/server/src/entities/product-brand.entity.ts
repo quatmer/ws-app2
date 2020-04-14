8f0bd4d2e09368433ff24bc9eb8aec20a630e012
Mehmet <iresyak@gmail.com>
2020-04-14 09:01:06 +0200

    s-imports updated and interface renamed

--- packages/server/src/entities/product-brand.entity.ts
+++ packages/server/src/entities/product-brand.entity.ts
@@ -1,6 +1,6 @@
 import { Schema, Document, model } from 'mongoose';
 import mongooseUniqueValidator = require('mongoose-unique-validator');
-import { IBrandDocument } from '@shared/models/product-brand';
+import { IProductBrandDocument } from '../../../shared/models/product-brand';
 
 export const ProductBrandSchema = new Schema(
   {
@@ -9,6 +9,6 @@ export const ProductBrandSchema = new Schema(
   { timestamps: true, versionKey: false, bufferCommands: false, collection: 'brands' },
 );
 
-export interface ProductBrandDocument extends IBrandDocument, Document {}
+export interface ProductBrandDocument extends IProductBrandDocument, Document {}
 
 export const ProductBrandEntity = model<ProductBrandDocument>('Brand', ProductBrandSchema);
