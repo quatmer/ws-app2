f3eae51ae94fe0a335834849c966362df120d562
Serdar YILMAZ <serdaryilmaz1323@gmail.com>
2020-04-14 14:44:57 +0200

    s-feat: create insert method in product-controller

--- packages/server/src/controllers/product.controller.ts
+++ packages/server/src/controllers/product.controller.ts
@@ -1,9 +1,30 @@
+import { ProductBrandEntity } from './../entities/product-brand.entity';
+import { IProductCategory } from '@shared/models/product-category';
 import { ProductEntity } from './../entities/product.entity';
 import { Request, Response, NextFunction } from 'express';
 import { HttpError } from '../util/HttpError';
+import { ProductCategoryEntity } from '../entities/product-category.entity';
 
 const insert = async (req: Request, res: Response, next: NextFunction) => {
-  const { name, description, unit, price, brand, categories } = req.body;
+  const { name, description, unit, price, brand: _brand, categories: _categories } = req.body;
+
+  console.log('Request body:', req.body);
+
+  //check categories
+  if (!_categories || _categories.length === 0) {
+    next(new HttpError('Categories cannot found', 500));
+    return;
+  }
+  const categories = await ProductCategoryEntity.find({ _id: _categories });
+  console.log({ cats: categories });
+
+  //check brand
+  const brand = await ProductBrandEntity.findById(_brand);
+  console.log('Brand:', brand);
+  if (!brand) {
+    next(new HttpError('Brand cannot found', 500));
+    return;
+  }
 
   let product = new ProductEntity();
   product.name = name;
