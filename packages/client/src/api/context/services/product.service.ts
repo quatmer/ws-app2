a3e2dfff4b99d3ba6477e4ddaba818201ad4cc7c
Mehmet <iresyak@gmail.com>
2020-04-15 15:44:23 +0200

    c-feat: product services created

--- packages/client/src/api/context/services/product.service.ts
+++ packages/client/src/api/context/services/product.service.ts
@@ -1,9 +1,9 @@
-import { IProduct } from '@shared/models/product';
-import { ProductActions } from './../../../redux/product/action';
 import Axios from 'axios';
+import { ProductActions } from './../../../redux/product/action';
 import { BaseService } from './base.service';
 import { IProductDTO } from '../../dto/product.dto';
 import { AppActions } from '../../../redux/app/action';
+import { IProduct } from '../../../../../shared/models/product';
 
 export class ProductService extends BaseService {
   create(newProduct: IProductDTO) {
@@ -29,50 +29,53 @@ export class ProductService extends BaseService {
     });
   }
 
-  // update(id: string, name: string) {
-  //   return new Promise<IProductBrand>(async (resolve, reject) => {
-  //     try {
-  //       const response = await Axios.post<{ brand: IProductBrand }>('/brand/' + id, { name });
-  //       const { brand } = response.data;
-  //       this.dispatch(BrandActions.update(brand));
-  //       this.dispatch(
-  //         AppActions.showNotification('Update brand', `Brand [${brand.name}] successfully updated`, 'information'),
-  //       );
-  //       resolve(brand);
-  //     } catch (error) {
-  //       const message = !!error.response ? error.response.statusText : error.message;
-  //       reject(message);
-  //     }
-  //   });
-  // }
+  update(uProduct: IProductDTO) {
+    return new Promise<IProduct>(async (resolve, reject) => {
+      try {
+        const response = await Axios.post<{ product: IProduct }>('/product/' + uProduct.id, { uProduct });
+        const { product } = response.data;
+        this.dispatch(ProductActions.update(product));
+        this.dispatch(
+          AppActions.showNotification(
+            'Update product',
+            `Product [${product.name}] successfully updated`,
+            'information',
+          ),
+        );
+        resolve(product);
+      } catch (error) {
+        const message = !!error.response ? error.response.statusText : error.message;
+        reject(message);
+      }
+    });
+  }
 
-  // delete(id: string) {
-  //   return new Promise(async (resolve, reject) => {
-  //     try {
-  //       await Axios.delete('/brand/' + id);
-  //       this.dispatch(BrandActions.delete(id));
-  //       this.dispatch(AppActions.showNotification('Delete brand', `Brand successfully deleted`, 'information'));
-  //       resolve();
-  //     } catch (error) {
-  //       const message = !!error.response ? error.response.statusText : error.message;
-  //       this.dispatch(AppActions.showNotification('Error on delete brand', message, 'danger'));
-  //       reject(message);
-  //     }
-  //   });
-  // }
+  delete(id: string) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        await Axios.delete('/product/' + id);
+        this.dispatch(ProductActions.delete(id));
+        this.dispatch(AppActions.showNotification('Delete product', `Product successfully deleted.`, 'information'));
+        resolve();
+      } catch (error) {
+        const message = !!error.response ? error.response.statusText : error.message;
+        this.dispatch(AppActions.showNotification('Error on delete product.', message, 'danger'));
+        reject(message);
+      }
+    });
+  }
 
-  // getList() {
-  //   return new Promise<IProductBrand[]>(async (resolve, reject) => {
-  //     try {
-  //       const response = await Axios.get<{ brands: IProductBrand[] }>('/brand', {});
-
-  //       this.dispatch(BrandActions.setList(response.data.brands));
-  //       resolve(response.data.brands);
-  //     } catch (error) {
-  //       const message = !!error.response ? error.response.statusText : error.message;
-  //       this.dispatch(AppActions.showNotification('Error on get list of brand', message, 'danger'));
-  //       reject(message);
-  //     }
-  //   });
-  // }
+  getList() {
+    return new Promise<IProduct[]>(async (resolve, reject) => {
+      try {
+        const response = await Axios.get<{ products: IProduct[] }>('/product', {});
+        this.dispatch(ProductActions.list(response.data.products));
+        resolve(response.data.products);
+      } catch (error) {
+        const message = !!error.response ? error.response.statusText : error.message;
+        this.dispatch(AppActions.showNotification('Error on get list of product.', message, 'danger'));
+        reject(message);
+      }
+    });
+  }
 }
