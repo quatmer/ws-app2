5237b0b3fbedc60530162dc20113d7008f17968b
Mehmet <iresyak@gmail.com>
2020-04-15 15:35:41 +0200

    c-feat: product state added

--- packages/client/src/redux/combine.ts
+++ packages/client/src/redux/combine.ts
@@ -5,12 +5,14 @@ import { combineReducers } from 'redux';
 import { authReducer } from './auth/reducer';
 import { productCategoryReducer } from './product-category/reducer';
 import { productCategoryEpics } from './product-category/epic';
+import { productReducer } from './product/reducer';
 
 export const rootEpic = combineEpics(...productCategoryEpics);
 
 export const rootReducer = combineReducers({
   authState: authReducer,
   appState: appReducer,
+  productState: productReducer,
   productCategoryState: productCategoryReducer,
   productBrandState: brandReducer,
 });
