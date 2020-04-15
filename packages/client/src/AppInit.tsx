372cd4e9dbc4cdc14ac96da103c3ad3f9f190874
Mehmet <iresyak@gmail.com>
2020-04-15 15:39:41 +0200

    c-feat: brand&category services added to AppInit

--- packages/client/src/AppInit.tsx
+++ packages/client/src/AppInit.tsx
@@ -4,11 +4,17 @@ import { IonApp } from '@ionic/react';
 import { AppService } from './api/services/app.service';
 import NotificationContainer from './container/Notification/NotificationContainer';
 import { useServices } from './api/context/ServiceContext';
+import { useDispatch } from 'react-redux';
+import { ProductCategoryActions } from './redux/product-category/action';
 
 const AppInit = () => {
-  const { Auth } = useServices();
+  const { Auth, BrandService } = useServices();
+  const dispatch = useDispatch();
+
   useEffect(() => {
     AppService.initApp(Auth);
+    BrandService.getList();
+    dispatch(ProductCategoryActions.getList());
     // eslint-disable-next-line
   }, []);
 
