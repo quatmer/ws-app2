8d331c79fd680f25c1c3ad1588a6126c6a7d6932
Serdar YILMAZ <serdaryilmaz1323@gmail.com>
2020-04-16 11:07:45 +0200

    c-fix: solved bugs in product -> reducer

--- packages/client/src/redux/product/reducer.ts
+++ packages/client/src/redux/product/reducer.ts
@@ -17,26 +17,29 @@ export const productReducer: Reducer<ProductStateType, ProductFuncType> = (
   switch (action.type) {
     case ProductActionType.ADD: {
       const { product } = action.payload;
-      state.products.push(product);
-      return { ...state };
+      const products = [...state.products];
+      products.push(product);
+      return { ...state, products };
     }
 
     case ProductActionType.UPDATE: {
       const { product } = action.payload;
-      let foundProduct = state.products.find(p => p._id === product._id);
-      if (foundProduct) {
-        foundProduct = product;
+      const products = [...state.products];
+      const idx = products.findIndex(p => p._id === product._id);
+      if (idx !== -1) {
+        products[idx] = product;
       }
-      return { ...state };
+      return { ...state, products };
     }
 
     case ProductActionType.DELETE: {
       const { id } = action.payload;
-      let productIndex = state.products.findIndex(p => p._id === id);
-      if (productIndex !== -1) {
-        state.products.splice(productIndex, 1);
+      const products = [...state.products];
+      let idx = products.findIndex(p => p._id === id);
+      if (idx !== -1) {
+        products.splice(idx, 1);
       }
-      return { ...state };
+      return { ...state, products };
     }
 
     case ProductActionType.LIST: {
