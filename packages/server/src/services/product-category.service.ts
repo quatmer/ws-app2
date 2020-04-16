6503ad7d605e7b6aa4681d67abfaa0a7c5721145
ahmetsametoglu.fr@gmail.com <ahmetsametoglu.fr@gmail.com>
2020-04-16 13:33:02 +0200

    s-feat: create decreaseProductCount function

--- packages/server/src/services/product-category.service.ts
+++ packages/server/src/services/product-category.service.ts
@@ -2,14 +2,31 @@ import { ProductCategoryEntity } from '../entities/product-category.entity';
 
 export const ProductCategoryService = {
   increaseProductCount: async (categoryIds: string[]) => {
-    try {
-      const response = await ProductCategoryEntity.updateMany(
-        { _id: { $in: categoryIds } },
-        { $inc: { productCount: 1 } },
-      );
-      console.log('RESPONSE on product Entity :', response);
-    } catch (error) {
-      console.log('ERROR ON Product Entity :', error);
-    }
+    return new Promise(async (resolve, reject) => {
+      try {
+        const response = await ProductCategoryEntity.updateMany(
+          { _id: { $in: categoryIds } },
+          { $inc: { productCount: 1 } },
+        );
+        if (response.n > 0) resolve();
+        else reject('Categories product count can not increase.');
+      } catch (error) {
+        reject(error.message);
+      }
+    });
+  },
+  decreaseProductCount: async (categoryIds: string[]) => {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const response = await ProductCategoryEntity.updateMany(
+          { _id: { $in: categoryIds } },
+          { $inc: { productCount: -1 } },
+        );
+        if (response.n > 0) resolve();
+        else reject('Categories product count can not increase.');
+      } catch (error) {
+        reject(error.message);
+      }
+    });
   },
 };
